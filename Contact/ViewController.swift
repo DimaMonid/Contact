40896c7343f77c56c4eddc65172d89b9b927c0ba
Dima Monid <1234567waka.waka@gmail.com>
2025-02-08 22:43:19 +0300

    Initial Commit

---
+++ Contact/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Contact
+//
+//  Created by Дима Монид on 8.02.25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
